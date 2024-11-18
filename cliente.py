48c2e4b30ab62f45fb60da8dbb6d151021cfe7b7
GabrielMocelinSander <134561846+GabrielMocelinSander@users.noreply.github.com>
2024-11-18 20:51:27 -0300

    cliente definitivo

--- cliente.py
+++ cliente.py
@@ -1,4 +1,5 @@
 import socket
+from time import sleep
 
 host = '10.1.24.153'  
 port = 12345 
@@ -12,12 +13,15 @@ print(f"Conectado ao servidor {host}:{port}")
 
 try:
 
-    message = "Olá, servidor!"
-    client_socket.send(message.encode())
+    while True:
+        message = "Olá, servidor!"
+        client_socket.send(message.encode())
 
 
-    data = client_socket.recv(1024)
-    print(f"Resposta do servidor: {data.decode()}")
+        data = client_socket.recv(1024)
+        print(f"Resposta do servidor: {data.decode()}")
+
+        sleep(1)
 
 except Exception as e:
     print(f"Ocorreu um erro: {e}")
